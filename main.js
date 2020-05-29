030d05d99f9dd72a8e286f509786a2b09957231a
pouyatirdad <66117587+pouyatirdad@users.noreply.github.com>
2020-05-29 16:08:57 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function zarb(form) {
+    var a=form.ip1.value;
+    var b=form.ip2.value;
+    document.getElementById('natije').innerHTML= a * b;
+}
+function taghsim(form) {
+    var a=form.ip1.value;
+    var b=form.ip2.value;
+    document.getElementById('natije').innerHTML= a / b;
+}
